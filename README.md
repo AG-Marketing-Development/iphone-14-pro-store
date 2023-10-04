e5190b0a5344792003b0c90bc813a9b0dc4fe41c
rigomoreirar <rigomoreirar@gmail.com>
2023-10-04 10:04:13 -0600

    Added React Project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # iphone-14-pro-store
 A React-based frontend for an online store specializing in iPhone 14 Pro sales.
+
+
+# This app uses node v20.6.0 and npm v9.8.1
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+
+
+  return (
+
+    <div>
+        <p>
+          Hi!
+        </p>
+    </div>
+
+  );
+};
+
+export default App;
